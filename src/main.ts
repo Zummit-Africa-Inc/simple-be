9fb8a90a6db8cedba65fc1b4e588c5b4c8baa773
unknown <babafemiolasunmade@gmal.com>
2022-04-25 11:49:26 +0100

    nest app created with all configurations needed for execution

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  if (process.env.NODE_ENV !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle('ZA Web BE - ZAPI')
+      .setDescription('Zummit Africa Web BackEnd ZAPI')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-doc', app, document);
+  }
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  await app.listen(Number(process.env.NODE_PORT) || 3000);
+}
+bootstrap();
